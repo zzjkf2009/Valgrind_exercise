8beea4e40ed9338019d4e21f5fb227159b65453c
Zejiang Zeng <zzeng@terpmail.umd.edu>
2017-10-03 19:51:56 -0700

    use smart pointer instead of raw pointer

--- app/AnalogSensor.cpp
+++ app/AnalogSensor.cpp
@@ -1,6 +1,7 @@
 #include <AnalogSensor.hpp>
 #include <numeric>
 #include <vector>
+#include <memory>
 
 AnalogSensor::AnalogSensor(unsigned int samples)
     : mSamples(samples)
@@ -13,10 +14,13 @@ AnalogSensor::~AnalogSensor()
 
 int AnalogSensor::Read()
 {
-    std::vector<int> *readings = new std::vector<int>(mSamples, 10);
+    /* Instead of using old version pointers without deleting them, it is safer
+       to use smart pointers */
+
+    //std::vector<int> *readings = new std::vector<int>(mSamples, 10);
+
+    std::unique_ptr<std::vector<int>> readings (new std::vector<int>(mSamples, 10));
 
     double result = std::accumulate( readings->begin(), readings->end(), 0.0 ) / readings->size();
     return result;
 }
-
-
